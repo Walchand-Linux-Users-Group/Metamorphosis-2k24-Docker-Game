acb7b568754e6612396298e0dd5e80f42be89ccd
jyotiradityz <patiljyotiraditya123@gmial.com>
2024-03-03 17:21:19 +0530

    ok

--- src/Submit.jsx
+++ src/Submit.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react' 
+import React, { useState } from 'react'
 import axios from 'axios'
 const Submit = () => {
     const [username, setUsername] = useState('')
     const [flag, setFlag] = useState('')
-
+    // const [responses, setResponses] = useState(0)
     const handleFormSubmit = (e) => {
         e.preventDefault()
         axios.post('https://dmeta2024games.onrender.com/api/v1/submit', {
@@ -11,11 +11,11 @@ const Submit = () => {
             flag: flag
         })
             .then((response) => {
-                console.log(response)
+                alert('Correct Flag')
             })
             .catch((error) => {
-                console.log(error)
-            })     
+                alert('Invalid Flag or invalid username')
+            })
     }
     const handlesUsername = (e) => {
         setUsername(e.target.value)
@@ -25,25 +25,27 @@ const Submit = () => {
     }
 
     return (
-        <div className="ring">
-            <i style={{ '--clr': '#d900ff' }}></i>
-            <i style={{ '--clr': '#1fbfdb' }}></i>
-            <i style={{ '--clr': '#a60074' }}></i>
-            <form action="submit" onSubmit={handleFormSubmit} className='login'>
-                <div className="login"   >
-                    <h2>CONTAINERS</h2>
-                    <div className="inputBx" onChange={handlesUsername}>
-                        <input type="text" placeholder="Username" />
+        <>
+            <div className="ring">
+                <i style={{ '--clr': '#d900ff' }}></i>
+                <i style={{ '--clr': '#1fbfdb' }}></i>
+                <i style={{ '--clr': '#a60074' }}></i>
+                <form action="submit" onSubmit={handleFormSubmit} className='login'>
+                    <div className="login"   >
+                        <h2>CONTAINERS</h2>
+                        <div className="inputBx" onChange={handlesUsername}>
+                            <input type="text" placeholder="Username" />
+                        </div>
+                        <div className="inputBx">
+                            <input type="text" placeholder="Flag" onChange={handleFlag} />
+                        </div>
+                        <div className="inputBx">
+                            <input type="submit" value="Submit Flag" />
+                        </div>
                     </div>
-                    <div className="inputBx">
-                        <input type="text" placeholder="Flag" onChange={handleFlag} />
-                    </div>
-                    <div className="inputBx">
-                        <input type="submit" value="Submit Flag" />
-                    </div>
-                </div>
-            </form>
-        </div>
+                </form>
+            </div>
+        </>
     )
 }
 
